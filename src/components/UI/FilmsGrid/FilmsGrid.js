afee12430ee9ff92c8d5878a23c45130aa556949
Francis Gette <franngette@gmail.com>
2021-02-25 22:33:39 -0300

    basic app structure

---
+++ src/components/UI/FilmsGrid/FilmsGrid.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+const FilmsGrid = (props) => {
+  return <div className={styles.wrapper}>Film Grid</div>;
+};
+
+export default FilmsGrid;
---
+++ src/components/UI/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./style.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div>ICONS</div>
+      <div>LOGO</div>
+      <div>SEARCH BAR</div>
+    </header>
+  );
+};
+
+export default Header;
